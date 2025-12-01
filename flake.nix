51a56f1aec4897618c5e3b1b9c14cb5af00293b5
Alexander Ou <alexou@femtodata.com>
2025-12-01 10:42:05 -0800

    flake update / lock, nixpkgs 25.11

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }@inputs: {
-    nixosModules = {
-      docker-compose = import ./modules/docker-compose.nix;
-      neovim-with-plugins = import ./modules/neovim-with-plugins.nix;
-      switch-fix = import ./modules/switch-fix.nix;
-      symlink = import ./modules/symlink.nix;
-      tmux-enhanced = import ./modules/tmux-enhanced.nix;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    {
+      nixosModules = {
+        docker-compose = import ./modules/docker-compose.nix;
+        neovim-with-plugins = import ./modules/neovim-with-plugins.nix;
+        switch-fix = import ./modules/switch-fix.nix;
+        symlink = import ./modules/symlink.nix;
+        tmux-enhanced = import ./modules/tmux-enhanced.nix;
+      };
     };
-  };
 }
